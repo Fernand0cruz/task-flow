c1b22cd89e502c26ca8b9a9d6390a5b54d26fc52
Fernando Cruz <cruzfernando.dev@gmail.com>
2025-10-22 18:50:19 -0300

    feat: add main application structure and header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
-    return <>home</>;
+    return (
+        <div className="app-container">
+            {/*Header */}
+            <Header />
+
+            {/*Controls*/}
+
+            {/*Task List*/}
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,14 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Sua Lista de Tarefas!</h1>
+
+            <div className="stats">
+                <span>✅Concluidas: xxx</span>
+                <span>🔄Pendentes: xxx</span>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
